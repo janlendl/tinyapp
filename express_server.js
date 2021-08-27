419da0a4dc097374aaca728bab093db0bc7bd445
Lendl Peralta <janlendl@gmail.com>
2021-08-27 03:47:34 +0000

    Add urls_new to render

--- express_server.js
+++ express_server.js
@@ -20,14 +20,14 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render('urls_show', templateVars);
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
 });
 
 
