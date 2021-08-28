6d8853cc3b360d5cb643465e4cd197b85cc62cad
Lendl Peralta <janlendl@gmail.com>
2021-08-28 04:58:35 +0000

    Update script to include nodemon run command

--- express_server.js
+++ express_server.js
@@ -53,5 +53,5 @@ app.get('/u/:shortURL', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`TinyURL app listening on port ${PORT}`);
 });
