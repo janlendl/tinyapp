2dac8ceb182f570c98c691b5daf929783a7eeaaa
Lendl Peralta <janlendl@gmail.com>
2021-08-28 01:27:52 +0000

    add function to redirect shortURL link to the website associated

--- express_server.js
+++ express_server.js
@@ -41,6 +41,11 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
