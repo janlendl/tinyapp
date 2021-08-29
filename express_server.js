bad11f66d4ef4ff6d131813c0520ab5e931b4ef1
Lendl Peralta <janlendl@gmail.com>
2021-08-29 23:28:53 +0000

    Call cookieParser

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const { render } = require('ejs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 3000;
 
